b0ab30c0c6b5fb8e4abf2f308ad8d8bc1c7970c8
sami1081 <2310717@iub.edu.bd>
2024-08-25 23:50:09 +0600

    Add files via upload

---
+++ question.js
@@ -0,0 +1,59 @@
+let questions = [
+  {
+      numb:1,
+      question:'What is The Capital City of Bangladesh?',
+      answer:'Dhaka',
+      options:[
+          "Khulna",
+          "Dhaka",
+          "Sylhet",
+          "Barisal"
+      ]
+  },
+  {
+      numb: 2,
+      question: "Which was the naval sector in liberation war?",
+      answer: "Sector 10",
+      options: [
+        "sector 1",
+        "Sector 5",
+        "Sector 8",
+        "Sector 10"
+      ]
+    },
+    {
+      numb: 3,
+      question: "He has ____ friends in Chicago.",
+      answer: "few",
+      options: [
+        "many",
+        "few",
+        "much",
+        "a lot of"
+      ]
+    },
+  
+    {
+      numb: 4,
+      question: "What Does HTML Stands for?",
+      answer: "Hyper Text Markup Language",
+      options: [
+            "Hyper Text Preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Language",
+            "Hyper Tool Multi Language"
+      ]
+    },
+
+    {
+      numb: 5,
+      question: "What is the currency of Bangladesh?",
+      answer: "Taka",
+      options: [
+        "Taka",
+        "Bath",
+        "Dollar",
+        "Euro"
+      ]
+    },
+]
